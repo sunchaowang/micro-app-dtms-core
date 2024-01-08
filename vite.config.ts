3220d870190d4d3fc080e51d3d9d77582b4bb752
sunchaowang <sunchaowang@outlook.com>
2024-01-08 18:12:09 +0800

    no message

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { UserConfig } from 'vite';
+import { userConfig as baseUserConfig, loadCurrentEnv } from '../../vite.config.base';
+import { wrapperEnv } from '@shared/utils';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-});
+export default ({ command, mode }): UserConfig => {
+  const root = process.cwd();
+
+  const currentEnv = loadCurrentEnv(command, root);
+  const env = wrapperEnv(currentEnv);
+
+  console.log('root', root, env);
+  return {
+    ...baseUserConfig,
+    resolve: {},
+    server: {
+      port: env.VITE_APP_PORT,
+    },
+  } as UserConfig;
+};
